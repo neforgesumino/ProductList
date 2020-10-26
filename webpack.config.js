5f472a33b46689622518760506a4b4d9e18101f0
neforgesumino <57845270+neforgesumino@users.noreply.github.com>
2020-10-26 21:32:50 +0300

    built with webpack
    final version

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin =  require('mini-css-extract-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', './App.js'],
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'public')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[id].css',
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test:/\.css$/,
+                use: [MiniCssExtractPlugin.loader,'css-loader', 'postcss-loader' ]
+            },
+            {
+                test:/\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(otf|eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/, 
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
